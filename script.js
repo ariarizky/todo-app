451fb81a20429e7a857f489a5bccd8d5b1d7f799
ariarizky <58224363+ariarizky@users.noreply.github.com>
2021-10-05 13:14:28 +0700

    menambah function clear todos

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const addForm = document.querySelector(".add-form");
 const addInput = document.querySelector(".add-input");
 const todos = document.querySelector(".todos");
+const clearBtn = document.querySelector("#clear-btn");
+console.log(clearBtn);
 
-addForm.addEventListener("submit", function (e) {
+addForm.addEventListener("submit", addTodo);
+clearBtn.addEventListener("click", clearTodos);
+
+function addTodo(e) {
   e.preventDefault();
-  const span = document.createElement("span");
-  span.className = "todo";
-  span.appendChild(document.createTextNode(addInput.value));
-  todos.appendChild(span);
-  console.log(span);
-});
+  if (addInput.value === "") {
+    alert("Input todo terlebih dahulu!!!");
+  } else {
+    const span = document.createElement("span");
+    span.className = "todo";
+    span.appendChild(document.createTextNode(addInput.value));
+    todos.appendChild(span);
+    addInput.value = "";
+  }
+}
+
+function clearTodos(e) {
+  e.preventDefault();
+  todos.innerHTML = "";
+}
